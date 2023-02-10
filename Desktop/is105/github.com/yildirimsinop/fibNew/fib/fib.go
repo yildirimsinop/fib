67d769e7188364b065b96c466ab92a1250b6ab69
Umit Yildirim <umity@uia.no>
2023-02-10 11:06:17 +0100

    Response6

---
+++ Desktop/is105/github.com/yildirimsinop/fibNew/fib/fib.go
@@ -0,0 +1,8 @@
+package fib
+
+func Fib(n int) int {
+	if n == 0 {
+		return 0
+	}
+	return 1
+}
---
+++ Desktop/is105/github.com/yildirimsinop/fibNew/fib/fib_test.go
@@ -0,0 +1,43 @@
+package fib
+
+import "testing"
+
+/*
+func TestFib(t *testing.T) {
+	input := 0
+	got := Fib(input)
+	want := 1
+	if got != want {
+		t.Errorf("Fib(%d): want %d, got %d", input, want, got)
+	}
+	input = 1 // legg merke til operatøren =
+	got = Fib(input)
+	want = 1
+	if got != want {
+		t.Errorf("Fib(%d): want %d, got %d", input, want, got)
+	}
+}
+*/
+
+func TestFib(t *testing.T) {
+	type test struct {
+		input int
+		want  int
+	}
+
+	var tests = []test{
+		{0, 0},
+		{1, 1},
+		{2, 1},
+		// her kan vi bare legge på nye test cases
+	}
+
+	// tc - test case
+	for _, tc := range tests {
+		got := Fib(tc.input)
+		if got != tc.want {
+			t.Errorf("Fib(%d): want %d, got %d", tc.input, tc.want, got)
+		}
+	}
+
+}
