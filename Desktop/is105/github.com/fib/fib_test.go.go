31c39cfd8536b7aaeb8ca465d2fe9066c1389a3d
Umit Yildirim <umity@uia.no>
2023-02-10 10:42:46 +0100

    here

--- Desktop/is105/github.com/fib/fib_test.go.go
+++ Desktop/is105/github.com/fib/fib_test.go.go
@@ -5,7 +5,7 @@ import "testing"
 func TestFib(t *testing.T) {
 	input := 0
 	got := Fib(input)
-	want := 1
+	want := 0
 	if got != want {
 		t.Errorf("Fib(%d): want %d, got %d", input, want, got)
 	}
